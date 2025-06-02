f5763e025451e07eda6c9a83c813b8624f78242a
wailsamjouni <waill.samjouni@gmail.com>
2025-06-02 12:26:45 +0200

    print some info line

--- src/dibs_cli/main.py
+++ src/dibs_cli/main.py
@@ -111,9 +111,10 @@ def simulate_all_building(
 
     with tqdm(total=1, desc="Writing summary result in ", colour='red') as pbar:
         start_time = time.time()
+        print(f"folder_path is : {folder_path}")
         summary_result_dataframe = build_all_results_of_all_buildings_to_dataframe(summary_result, file_name)
         summary_result_dataframe.to_excel(rf"{folder_path}/annualResults_summary.xlsx", index=False)
-        print(f"folder_path is : {folder_path}")
+
         end_time = time.time()
         saving_summary_result = end_time - start_time
         pbar.update(1)
